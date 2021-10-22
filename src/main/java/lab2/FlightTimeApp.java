cf8087c4d5c670c4d582db6decf015dc38ad6be7
alina-torr <toropchina_a1@mail.ru>
2021-10-23 01:26:54 +0300

    Scripted auto-commit on change (2021-10-23 01:26:54) by gitwatch.sh

--- src/main/java/lab2/FlightTimeApp.java
+++ src/main/java/lab2/FlightTimeApp.java
@@ -1,9 +1,11 @@
 package lab2;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
@@ -19,13 +21,13 @@ public class FlightTimeApp {
         System.out.println("aaa");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
-        //FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        //job.setPartitionerClass(TextPair.FirstPartitioner.class);
-        //job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
         job.setReducerClass(JoinReducer.class);
-        //job.setMapOutputKeyClass(TextPair.class);
-        //job.setOutputKeyClass(Text.class);
-        //job.setOutputValueClass(Text.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
         //System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
