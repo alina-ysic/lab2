ce735cf9043b2c714c8cd22e0ff5a9c3c5ef121c
alina-torr <toropchina_a1@mail.ru>
2021-10-23 02:03:43 +0300

    Scripted auto-commit on change (2021-10-23 02:03:43) by gitwatch.sh

--- src/main/java/lab2/AirportMapper.java
+++ src/main/java/lab2/AirportMapper.java
@@ -7,9 +7,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class AirportMapper extends Mapper<Text, Text, Text, IntWritable> {
+public class AirportMapper extends Mapper<Text, Text, Text, Text> {
     @Override
-    protected void map(Text key, Text value, Mapper<Text, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+    protected void map(Text key, Text value, Mapper<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
         System.out.println("A " + key);
         context.write(new Text("a"), new Text("2"));
         super.map(key, value, context);
