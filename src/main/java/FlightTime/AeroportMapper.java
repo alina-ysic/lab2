35346768963fd688fc0e5336bd62232687f9d51a
alina-torr <toropchina_a1@mail.ru>
2021-10-23 00:44:48 +0300

    Scripted auto-commit on change (2021-10-23 00:44:48) by gitwatch.sh

--- src/main/java/FlightTime/AeroportMapper.java
+++ src/main/java/FlightTime/AeroportMapper.java
@@ -1,4 +1,9 @@
 package FlightTime;
 
-public class AeroportMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class AeroportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 }
