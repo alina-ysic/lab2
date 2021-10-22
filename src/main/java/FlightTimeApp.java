b694799789f5982ce03de71d384f42b70cfb5596
alina-torr <toropchina_a1@mail.ru>
2021-10-22 22:56:07 +0300

    Scripted auto-commit on change (2021-10-22 22:56:07) by gitwatch.sh

--- src/main/java/FlightTimeApp.java
+++ src/main/java/FlightTimeApp.java
@@ -2,6 +2,7 @@ public class FlightTimeApp {
 
 
     public static void main(String[] args) {
-        
+        JobConf conf = new JobConf(JoinJob.class);
+        conf.setJobName("flitht time");
     }
 }
