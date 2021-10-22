74f5c361454aa1b395333200e38d9dd5ef3b29b4
alina-torr <toropchina_a1@mail.ru>
2021-10-23 01:32:02 +0300

    Scripted auto-commit on change (2021-10-23 01:32:02) by gitwatch.sh

--- src/main/java/lab2/FlightTimeApp.java
+++ src/main/java/lab2/FlightTimeApp.java
@@ -10,7 +10,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.io.IOException;
 
 public class FlightTimeApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
         /*if (args.length != 3) {
             System.err.println("Usage: FlightTimeApp <path> <path>");
             System.exit(-1);
@@ -22,14 +22,14 @@ public class FlightTimeApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        //job.setPartitionerClass(TextPair.FirstPartitioner.class);
-        //job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setPartitionerClass(null);
+        job.setGroupingComparatorClass(null);
         job.setReducerClass(JoinReducer.class);
-        //job.setMapOutputKeyClass(TextPair.class);
+        job.setMapOutputKeyClass(null);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
-        //System.exit(job.waitForCompletion(true) ? 0 : 1);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
 }
