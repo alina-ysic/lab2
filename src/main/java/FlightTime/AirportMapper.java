feb247fedb77da956e1fdc43e08d4b0ed371c187
alina-torr <toropchina_a1@mail.ru>
2021-10-23 00:48:02 +0300

    Scripted auto-commit on change (2021-10-23 00:48:02) by gitwatch.sh

--- src/main/java/FlightTime/AirportMapper.java
+++ src/main/java/FlightTime/AirportMapper.java
@@ -5,5 +5,11 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
