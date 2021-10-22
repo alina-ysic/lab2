541bea0aa5ac5ebaead5d1da861594288b9c57c8
alina-torr <toropchina_a1@mail.ru>
2021-10-23 00:54:22 +0300

    Scripted auto-commit on change (2021-10-23 00:54:21) by gitwatch.sh

--- src/main/java/FlightTime/FlightTimeApp.java
+++ src/main/java/FlightTime/FlightTimeApp.java
@@ -2,7 +2,7 @@ package FlightTime;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.lib.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
 
 import java.io.IOException;
