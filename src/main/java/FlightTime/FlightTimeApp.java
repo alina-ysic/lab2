3aacfad1289555ea85580d148af0e1ff8d0901ab
alina-torr <toropchina_a1@mail.ru>
2021-10-23 00:58:26 +0300

    Scripted auto-commit on change (2021-10-23 00:58:26) by gitwatch.sh

--- src/main/java/FlightTime/FlightTimeApp.java
+++ src/main/java/FlightTime/FlightTimeApp.java
@@ -20,6 +20,14 @@ public class FlightTimeApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        //job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        //job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        //job.setReducerClass(JoinReducer.class);
+        //job.setMapOutputKeyClass(TextPair.class);
+        //job.setOutputKeyClass(Text.class);
+        //job.setOutputValueClass(Text.class);
+        //job.setNumReduceTasks(2);
+        //System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
 }
