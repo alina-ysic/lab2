3b0effef9bedc4ddfb35d1bc0baab9af3fc1c5d0
alina-torr <toropchina_a1@mail.ru>
2021-10-23 00:39:40 +0300

    Scripted auto-commit on change (2021-10-23 00:39:40) by gitwatch.sh

--- src/main/java/FlightTimeApp.java
+++ src/main/java/FlightTimeApp.java
@@ -1,13 +1,14 @@
+import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
 
 import java.io.IOException;
 
 public class FlightTimeApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Job job = Job.getInstance();
-        job.setJarByClass()
+        job.setJarByClass(FlightTimeApp.class);
+        job.setJobName("Flight Time");
+        MultipleInputs.addInputPath(job, new Path(args[0]));
     }
 
-    public FlightTimeApp() throws IOException {
-    }
 }
