8a760b2d777fb90edad6ac74a85200fbeebab864
alina-torr <toropchina_a1@mail.ru>
2021-10-23 00:33:09 +0300

    Scripted auto-commit on change (2021-10-23 00:33:09) by gitwatch.sh

--- src/main/java/FlightTimeApp.java
+++ src/main/java/FlightTimeApp.java
@@ -1,5 +1,10 @@
 import org.apache.hadoop.mapreduce.Job;
 
+import java.io.IOException;
+
 public class FlightTimeApp {
     Job job = Job.getInstance();
+
+    public FlightTimeApp() throws IOException {
+    }
 }
